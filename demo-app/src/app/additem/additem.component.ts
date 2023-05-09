7728763f00128b3fc06f89091df7d6423d9eb0b6
Sandipbhinge8170 <sandipbhinge8170@gmail.com>
2023-05-10 01:23:46 +0530

    changes dene
    changes dene

--- demo-app/src/app/additem/additem.component.ts
+++ demo-app/src/app/additem/additem.component.ts
@@ -6,24 +6,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./additem.component.css']
 })
 export class AdditemComponent implements OnInit {
-
-  constructor(private http:HttpClient) { }
-
-  ngOnInit() {
-    
-    let auth_token = "WebAuth1234...";
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${auth_token}`
-    });
-    const requestOptions1 = { headers: headers };
-  const requestOptions = { headers: headers };
-   
-  this.http
-      .get('https://www.aspiresoft.co.ke/Wt/APIs.aspx/additem', requestOptions)
-      .subscribe((res: any) => {
-          console.log(res);
-      });
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
-}
+
+  }
+
+
